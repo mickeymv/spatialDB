93886f82f41f63329a95b7e7530b21df9c16c93e
glennbright <glennbright@ufl.edu>
2015-10-08 16:05:42 -0400

    adding header and c files

---
+++ ConvexHull.c
@@ -0,0 +1,31 @@
+#ifndef CONVEXHULL_H
+#define CONVEXHULL_H
+
+public static Region2D ConvexHull::computeConvexHull(Point2D complex_point){
+
+	Region2D convexHull;
+
+	/*implementation*/
+
+	return convexHull;
+}
+
+public static Region2D ConvexHull::computeConvexHull(Region2D region){
+
+	Point2D complex_point;
+
+	/*change into complex point*/
+
+	return computeConvexHull(complex_point);
+}
+
+public static Region2D ConvexHull::computeConvexHull(Line2D complex_line){
+
+	Point2D complex_point;
+
+	/*change into complex point*/
+
+	return computeConvexHull(complex_point);
+}
+ 
+#endif
