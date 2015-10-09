edf216a20a64bcd54bbdb302c4dfd19e74f70624
glennbright <glennbright@ufl.edu>
2015-10-08 22:45:44 -0400

    resolving header file issue

---
+++ ConvexHull.h
@@ -0,0 +1,14 @@
+#ifndef CONVEXHULL_H
+#define CONVEXHULL_H
+
+/* provides static class ConvexHull with overloaded methods compute convex hull */
+
+class ConvexHull {
+	public static Region2D computeConvexHull(Point2D complex_point){}
+
+	public static Region2D computeConvexHull(Region2D region){}
+
+	public static Region2D computeConvexHull(Line2D complex_line){}
+};
+ 
+#endif
