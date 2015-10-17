3f01249d2990502b7821f107cf124ae1dab6d025
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2015-10-17 18:38:19 -0400

    Changes based on Prof's suggestions. 	-	Standardized header with group number and topic included in each file. 	-	Changed indents and braces styling (Java) to match to Prof's preference (C++).

--- source/MainClass.cpp
+++ source/MainClass.cpp
@@ -1,20 +1,18 @@
-#include "spatialoperations.h"  
+#include "Group3SpatialOperations.h" 
 #include <stdio.h>
 
-
-
 int main()
-	{  
+{ 
 
-	Point2D point;
- 	Line2D line;
-	Region2D region;
+  Point2D point;
+  Line2D line;
+  Region2D region;
 
-	point = getSpatialIntersection(point, point);
-	line = getSpatialUnion(line, line);
-	region = getSpatialDifference(region, region);
+  point = getSpatialIntersection(point, point);
+  line = getSpatialUnion(line, line);
+  region = getSpatialDifference(region, region);
 
-	printf("\nSuccessfully run!\n");
+  printf("\nSuccessfully ran Group 3's Spatial Operations Program!\n\n");
 
-	return 0;
-	}
+  return 0;
+}
