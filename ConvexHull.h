ee146f044734c878b9f9b53941b80292c0b773e6
glennbright <glennbright@ufl.edu>
2015-10-20 15:23:20 -0400

    added missing functions in the .h

--- ConvexHull.h
+++ ConvexHull.h
@@ -9,7 +9,15 @@
 
 class ConvexHull {
 	public:
-		static Region2D computeConvexHull(Point2D complex_point);
+		static Region2D computeJarvis(Point2D complex_point);
+
+		static Region2D computeGraham(Point2D complex_point);
+
+		static Region2D computeQuick(Point2D complex_point);
+
+		static Region2D computeDivideConquer(Point2D complex_point);
+
+		static Region2D computeNewAlgo(Point2D complex_point);
 };
 
 #endif
