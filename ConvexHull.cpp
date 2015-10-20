27b79916e6bdd828b8be81a393928bc2ee61fc30
jainayushi <ayushijain@ufl.edu>
2015-10-19 23:49:16 -0400

    Updated interfaces

---
+++ ConvexHull.cpp
@@ -0,0 +1,49 @@
+#include "ConvexHull.h"
+#include "Dummy.h"
+
+using namespace std;
+
+Region2D ConvexHull::computeJarvis(Point2D complex_point)
+{
+	Region2D convexHull;
+
+	/*implementation*/
+
+	return convexHull;
+}
+
+Region2D ConvexHull::computeGraham(Point2D complex_point)
+{
+	Region2D convexHull;
+
+	/*implementation*/
+
+	return convexHull;
+}
+
+Region2D ConvexHull::computeQuick(Point2D complex_point)
+{
+	Region2D convexHull;
+
+	/*implementation*/
+
+	return convexHull;
+}
+
+Region2D ConvexHull::computeDivideConquer(Point2D complex_point)
+{
+	Region2D convexHull;
+
+	/*implementation*/
+
+	return convexHull;
+}
+
+Region2D ConvexHull::computeNewAlgo(Point2D complex_point)
+{
+	Region2D convexHull;
+
+	/*implementation*/
+
+	return convexHull;
+}
