ac6bce9f5fd8f0799d3e7f0bc33a2500dc4bc810
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2015-11-09 17:43:59 -0500

    - Remove the old implementations of IPOT since we have the new ParallelObjectTraversal class. - Show an example of how the PlaneSweep and ParallelObjectTraversal classes could be used to implement Spatial and Topological operations. - TODO for Group2: Separate implementation aspects of Topological predicates from the common classes (PlaneSweep and ParallelObjectTraversal). Case in point being the topological vector arrays (vF, vG) and the Explore functions.

--- source/planesweep project/main.cpp
+++ source/planesweep project/main.cpp
@@ -17,13 +17,34 @@ int main(int argc, char *argv[]) {
     Point2D G;
     PlaneSweep ps(F, G);
 
+	/** Give an example below of how we can use the PlaneSweep and ParallelObjectTraversal
+	*	object and methods to implement Spatial Operations or Topological Predicates.	
+	*/
+
+	ps.selectFirst(); //Called only once to initialize the plane sweep variables object 
+	// and status.
+	
+	while (ps.getObject != none) {
+	 	//do some work related to spatial/topological operations
+	 	//use the 'object' or 'status' variables or the other functions
+	 	// such as getEvent, lookAhead() from the ps object
+	 	ps.selectNext();
+	}
+	
+	/** Example above.
+	*/
+	
 
     // *** begin test temp
     // for testing, I have hardcoded in "POT_Point2DPoint2D.cpp" the value vF[poi_shared] = true
     cout << "F and G = Point2D objects \n";
+    
+    //The below vF should be removed from the common PlaneSweep and ParallelObjectTraversal work.
     cout << " vF[poi_disjoint] = " << ps.getVF()[POT_Point2DPoint2D::poi_disjoint] << " \n";
     cout << " vF[poi_shared] = " << ps.getVF()[POT_Point2DPoint2D::poi_shared] << " \n";
     cout << "vector size = " << ps.getVFGSize() << " \n";
+    //The above vF should be removed from the common PlaneSweep and ParallelObjectTraversal work.
+    
     // *** end test temp
 
 
