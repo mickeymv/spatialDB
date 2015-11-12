6dd553ca67be907e3a9b03c34a062a3d30646cec
Mickey Vellukunnel <mickey.vellukunnel@gmail.com>
2015-11-12 13:52:00 -0500

    First draft of the ObjectIterator interface class which could be used to generalize the selectFirst and selectNext functions.

---
+++ source/planesweep project/ObjectIterator.h
@@ -0,0 +1,34 @@
+/**This is a pure virtual class/ abstract base class/Interface class which will have no objects of its own.
+* It's upto the derived classes; Poi, Line and RegionIterators to have definitions for 
+* the methods in this class.
+*/
+
+class ObjectIterator {
+
+public:
+
+        // Predicate that tests whether a constant iterator is empty.
+        virtual bool isEmpty() const = 0;
+
+
+        // Increment/decrement operators '++', '--'
+        virtual ObjectIterator &operator++() = 0;   // prefix
+        virtual ObjectIterator operator++(int postfix) = 0; // postfix
+        virtual ObjectIterator &operator--() = 0;   // prefix
+        virtual ObjectIterator operator--(int postfix) = 0; // postfix
+
+        // Comparison operators that compares a constant poi iterator position
+        // with another const poi iterator position "rhs"
+        virtual bool operator==(const ConstPoiIterator &rhs) const = 0;
+
+        virtual bool operator!=(const ConstPoiIterator &rhs) const = 0;
+
+        virtual bool operator<(const ConstPoiIterator &rhs) const = 0;
+
+        virtual bool operator<=(const ConstPoiIterator &rhs) const = 0;
+
+        virtual bool operator>(const ConstPoiIterator &rhs) const = 0;
+
+        virtual bool operator>=(const ConstPoiIterator &rhs) const = 0;
+
+    };
