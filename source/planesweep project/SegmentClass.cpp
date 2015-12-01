71579026d00a67a5789360121b15021253ef32bf
natashamandal <nuttymandal93@gmail.com>
2015-12-01 14:40:42 -0500

    Modularized Segment Class i.e. separated code from interface

--- source/planesweep project/SegmentClass.cpp
+++ source/planesweep project/SegmentClass.cpp
@@ -3,3 +3,40 @@
 //
 
 #include "SegmentClass.h"
+
+SegmentClass::SegmentClass()
+{
+    //default
+    lowerOrRight = 0;
+    upperOrLeft = 0;
+}
+
+SegmentClass::SegmentClass(int lOrR, int uOrL)
+{
+    lowerOrRight = lOrR;
+    upperOrLeft = uOrL;
+}
+
+int SegmentClass::getLowerOrRight()
+{
+  return lowerOrRight;
+}
+
+int SegmentClass::getUpperOrLeft()
+{
+  return upperOrLeft;
+}
+
+void SegmentClass::setLowerOrRight(int lOrR)
+{
+  lowerOrRight = lOrR;
+}
+
+void SegmentClass::setUpperOrLeft(int uOrL)
+{
+  upperOrLeft = uOrL;
+}
+
+SegmentClass::~SegmentClass()
+{
+}
