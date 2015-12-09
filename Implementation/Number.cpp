06cc73b73ca336fbcc31d48b8718dd55b278b804
Namrata Choudhury <namratachoudhury123@gmail.com>
2015-12-09 14:29:18 -0500

    fixed integration errors in Region2D

--- Implementation/Number.cpp
+++ Implementation/Number.cpp
@@ -1,32 +1,85 @@
 #include "Number.h"
+#include "BigRational.h"
 
-struct PrivateRec
+struct Number::PrivateRec
 {
-	float value;
+	BigRational value;
+	//float value;
 };
 Number::Number()
 {
 	p = new PrivateRec;
-	p->value = 0;
+	p->value = BigRational("0", "1");
 }
-Number::Number(float value)
+
+Number::Number(std::string value)
 {
-	float val = value;
+	int count = 0;
+	std::string num = "", den = "1";
+	for (int i = 0; i < value.length(); i++)
+	{
+		if (value.at(i) == '.')
+		{
+			count++;
+			if (count>1)
+			{
+				std::cout << "wrong input";
+				return;
+			}
+			//den = "1";
+		}
+		else
+		{
+			num += value.at(i);
+			if (count == 1)
+			{
+				den += "0";
+			}
+		}
+	}
+	this->p = new PrivateRec;
+	this->p->value = BigRational(num, den);
+	/*float val = std::stof(value);
 	p = new PrivateRec;
-	p->value = val;
+	p->value = val;*/
+
 }
+
 Number::Number(const Number& obj)
 {
+	this->p = new PrivateRec;
+	this->p->value = obj.p->value;
+}
 
+Number::Number(Number&& obj)
+{
+	this->p = new PrivateRec;
+	this->p->value = obj.p->value;
+	obj.p->value = BigRational("0", "1");
 }
 
 Number Number::operator=(const Number& obj)
 {
-	this->p->value = obj.p->value;
+	if (this != &obj)
+	{
+		this->p->value = obj.p->value;
+	}
 	return *this;
 }
+
+Number Number::operator=(Number&& obj)
+{
+	if (this != &obj)
+	{
+		this->p->value = obj.p->value;
+		obj.p->value = BigRational("0", "1");
+	}
+	return *this;
+}
+
 Number::~Number()
 {
+	delete p;
 }
 
 /*MATHEMATICAL OPERATIONS*/
