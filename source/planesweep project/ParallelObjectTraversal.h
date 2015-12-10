5d574bc749ac067009a9bdc1c781f6e13e6cee69
cocodtj <cocodtj@users.noreply.github.com>
2015-12-10 12:38:54 -0500

    ObjectIterator removal changes.

--- source/planesweep project/ParallelObjectTraversal.h
+++ source/planesweep project/ParallelObjectTraversal.h
@@ -19,9 +19,7 @@
 class ParallelObjectTraversal {
 
 public:
-    ParallelObjectTraversal(const Object2D &, const Object2D &);
-
-    ParallelObjectTraversal(const Poi2D &, const Poi2D &);
+    ParallelObjectTraversal(Object2D &, Object2D &);
 
     ~ParallelObjectTraversal();
 
@@ -33,13 +31,16 @@ public:
 
     ObjectIterator *getObjIterator(object);
 	// need to finalise the return type
-	AttrHalfSeg2D getNextMin();
+	void setNextMin();
 
     bool isObjectF(Object2D);
     bool isObjectG(Object2D);
 
-    ObjectIterator *getNextObjIterator(HalfSeg2D,object);
-    ObjectIterator *getNextObjIterator(AttrHalfSeg2D,object);
+    Object2D getObjF();
+    Object2D getObjG();
+
+    ObjectIterator* getNextObjIterator(HalfSeg2D,object);
+    ObjectIterator* getNextObjIterator(AttrHalfSeg2D,object);
 
     /*Checks whether the given segments are within an object
    */
@@ -55,7 +56,17 @@ public:
         end_of_first, end_of_second, end_of_both
     } status;
 
-    Poi2D getEvent(object objectEnumVal);
+    Poi2D getPoiEvent(object objectEnumVal);
+    HalfSeg2D getHalfSegEvent(object objectEnumVal);
+    AttrHalfSeg2D getAttrHalfSegEvent(object objectEnumVal);
+
+    Poi2D* getNextPoi2DMin();
+    HalfSeg2D* getNextHalfSeg2DMin();
+    AttrHalfSeg2D* getNextAttrHalfSeg2DMin();
+    Poi2D *minPoi2DF = nullptr,*minPoi2DG = nullptr;
+    HalfSeg2D *minHalfSeg2DF = nullptr, *minHalfSeg2DG= nullptr;
+    AttrHalfSeg2D *minAttrHalfSeg2DF= nullptr, *minAttrHalfSeg2DG= nullptr;
+
 
 private:
     //SelectFirst should be called in the POT constructor.
@@ -65,7 +76,10 @@ private:
     object object_value;
     status status_value;
     Object2D *objF, *objG;
-    ObjectIterator *objFIterator, *objGIterator;
+
+    Point2D::ConstPoiIterator * objFpoiIterator = nullptr, * objGpoiIterator = nullptr; // DTj Dec 5, 2015
+    Line2D::ConstSegIterator * objFsegIterator = nullptr, * objGsegIterator = nullptr; 
+    Region2D::ConstRegionIterator * objFregionIterator = nullptr, * objGregionIterator = nullptr; 
 
 };
 
