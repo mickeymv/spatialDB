40c0f89a725a455d1be38e542a89c56d7878a998
cocodtj <cocodtj@users.noreply.github.com>
2015-12-13 17:51:25 -0500

    do not use 'const'
    For all isXXXX, 'const' omitted.

--- source/planesweep project/Object2D.cpp
+++ source/planesweep project/Object2D.cpp
@@ -9,29 +9,16 @@
 
 
 // Equal operator that checks if the Object2D object and the operand Object2D are the same
-bool Object2D::operator == (const Object2D& operand)const {
+bool Object2D::operator == (Object2D& operand){
     if(this==&operand)
         return true;
     else
         return false;
 }
 
-bool Object2D::isPoint2D() const
+bool Object2D::isPoint2D()
 {
- if(dynamic_cast<const Point2D*>(this))
- {
-     return true;
- }
- else
- {
-     return false;
- }
-}
-
-bool Object2D::isLine2D() const
-{
-
-    if(dynamic_cast<const Line2D*>(this))
+    if(dynamic_cast<Point2D*>(this))
     {
         return true;
     }
@@ -41,9 +28,21 @@ bool Object2D::isLine2D() const
     }
 }
 
-bool Object2D::isRegion2D() const
+bool Object2D::isLine2D()
 {
-    if(dynamic_cast<const Region2D*>(this))
+    if(dynamic_cast<Line2D*>(this))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool Object2D::isRegion2D()
+{
+    if(dynamic_cast<Region2D*>(this))
     {
         return true;
     }
