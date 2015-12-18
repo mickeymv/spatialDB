9e3584f8bd0d4b7ca973d99cdf7952bd81a97810
cocodtj <cocodtj@users.noreply.github.com>
2015-12-18 03:43:04 -0500

    All Test Cases are now in separate classes, called from main.cpp
    All Test Cases are now in separate classes, called from main.cpp

---
+++ source/Topic4_compiled_ready/source/Topological_classes/TestPoint2DLine2D.h
@@ -0,0 +1,38 @@
+//
+// Created by Djundi on 12/18/15.
+//
+
+#ifndef TOPOLOGICAL_CLASSES_TESTPOINT2DLINE2D_H
+#define TOPOLOGICAL_CLASSES_TESTPOINT2DLINE2D_H
+
+#include "TopologicalRelationships.h"
+#include "vector"
+
+
+class TestPoint2DLine2D {
+
+
+public:
+    TestPoint2DLine2D() { }
+
+    virtual ~TestPoint2DLine2D() { }
+
+    void start();
+
+private:
+
+    string topPredNumberPoint2DLine2D[14] =
+            {
+                    "pl_disjoint_m1", "pl_disjoint_m2", "pl_meet_m3", "pl_meet_m4", "pl_meet_m5",
+                    "pl_meet_m6", "pl_inside_m7", "pl_inside_m8", "pl_overlap_m9", "pl_overlap_m10",
+                    "pl_inside_m11", "pl_inside_m12", "pl_overlap_m13", "pl_overlap_m14"
+            };
+
+    void printPoint2DLine2DPredicate(TopPredNumberPoint2DLine2D tpred) {
+
+        cout << topPredNumberPoint2DLine2D[(unsigned int) tpred] << " <===";
+    };
+};
+
+
+#endif //TOPOLOGICAL_CLASSES_TESTPOINT2DLINE2D_H
